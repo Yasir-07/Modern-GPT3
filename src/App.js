bedfdb92d44d7442a221b00e8d30df7bc52f09cb
Mohammed Yasir <mohammedyasir@Yasir.local>
2024-07-19 22:35:54 -0400

    modern gpt3

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import {
+  Blog,
+  Footer,
+  Header,
+  Possibility,
+  WhatGPT3,
+  Features,
+} from "./containers";
+import { CTA, Brand, Navbar } from "./components";
+import "./App.css";
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
